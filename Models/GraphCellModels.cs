76ece20b3189c727ef7d298aab895e3f7e6ce8ad
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2023-12-07 17:10:29 +0530

    Baba Graph Se khel Raha hy (BFS)

---
+++ Models/GraphCellModels.cs
@@ -0,0 +1,16 @@
+﻿namespace GettingStarted.Models
+{
+    public class GraphCellModels
+    {
+        public int XAxis { get; set; }
+        public int YAxis { get; set; }
+
+        public string  CellColor { get; set; }
+
+        public bool StartingPoint { get; set; } = false;
+
+        public bool EndingPoint { get; set;} = false;
+
+        public bool MyProperty { get; set; }=true;
+    }
+}
