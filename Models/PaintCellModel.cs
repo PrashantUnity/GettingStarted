cd8b37560a9070b10ba09b557c8b8a37ffa480cc
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2023-12-08 09:00:11 +0530

    Baba ne Sab beghar Ko Name Diya, ghar diya

---
+++ Models/PaintCellModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GettingStarted.Models
+{
+    public class PaintCellModel
+    {
+        public int XAxis { get; set; }
+        public int YAxis { get; set; }
+
+        public string CellColor { get; set; }
+
+        public string PreviousCellColor { get; set; } = "white";
+
+    }
+}
