07b68d0b1541e27fbc3b4980bb70c58a79c701d2
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2023-12-09 20:40:28 +0530

    Aaaye aap sab ke samne Buggy maze padhar rahae hai 🎶🎶

---
+++ Models/MazeCellModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GettingStarted.Models
+{
+    public class MazeCellModel
+    {
+        public int XAxis { get; set; }
+        public int YAxis { get; set; }
+        public int Count { get; set; } = 4;
+        public string CellColor { get; set; } = "black";
+
+        public List<MazeCellModel> Neighbour { get; set; } = new();
+    }
+}
