9221f6631f7b2d5f42a5777fd0e37890c401f593
PrashantUnity <48068632+PrashantUnity@users.noreply.github.com>
2023-12-10 16:22:05 +0000

    Deploying to gh-pages from  @ 2f8b088e15d74740ba9ec91052e1951339f8c4c1 🚀

--- js/githubgist.js
+++ js/githubgist.js
@@ -4,14 +4,17 @@
     const iframeId = `${userId}-${filename}`;
     iframe.setAttribute("id", iframeId);
     iframe.setAttribute("width", "100%");
+    iframe.setAttribute("frameborder", "0");
     target.appendChild(iframe);
 
     let doc = iframe.document;
     if (iframe.contentDocument) doc = iframe.contentDocument;
     else if (iframe.contentWindow) doc = iframe.contentWindow.document;
+    doc = iframe.contentDocument || iframe.contentWindow.document;
 
     const gistScript = `<script src="https://gist.github.com/${userId}/${filename}.js"></script>`;
     const resizeScript = `onload="parent.document.getElementById('${iframeId}').style.height=document.body.scrollHeight + 'px'"`;
+     
     const iframeHtml = `<html><body ${resizeScript}>${gistScript}</body></html>`;
 
     doc.open();
