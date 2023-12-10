2f8b088e15d74740ba9ec91052e1951339f8c4c1
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2023-12-10 21:51:09 +0530

    Two Sum Introducing Using Two Loop Later i will update with different more intuitive approach

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using GettingStarted;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting; 
+using MudBlazor.Services; 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+ 
 
 await builder.Build().RunAsync();
