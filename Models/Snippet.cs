adeafd7bd0907782d9ba7b1d1eb06f622ace7553
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2023-12-13 15:35:55 +0530

    Quick Language string transformation

--- Models/Snippet.cs
+++ Models/Snippet.cs
@@ -6,8 +6,8 @@
         public string BoderColor { get; set; } = "black";
         public int BoderThickNess { get; set; } = 0; 
         public int HeightOfFrame { get; set; } = 500;
-        public string Language { get; set; } = Enum.GetName(LanguageName.CSharp);
+        public string? Language { get; set; } = Enum.GetName(LanguageName.CSharp);
 
-        public string SourceUrl { get; set; } = "https://pastebin.com/embed_iframe/YBZtRsfb";
+        public string? SourceUrl { get; set; } = "https://pastebin.com/embed_iframe/YBZtRsfb";
     }
 }
