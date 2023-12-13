fe6106fb5204bb92e6a9a18c5d454288b3892ac8
PrashantUnity <48068632+PrashantUnity@users.noreply.github.com>
2023-12-13 10:06:56 +0000

    Deploying to gh-pages from  @ adeafd7bd0907782d9ba7b1d1eb06f622ace7553 🚀

---
+++ copyCode.js
@@ -0,0 +1,18 @@
+﻿window.copyCode = function (text) {
+
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    textarea.style.position = 'fixed';  // To prevent scrolling when focusing on the textarea
+    document.body.appendChild(textarea);
+    textarea.select();
+
+    try {
+        const successful = document.execCommand('copy');
+        const message = successful ? 'Text copied to clipboard' : 'Unable to copy text';
+        console.log(message);
+    } catch (err) {
+        console.error('Error copying text:', err);
+    }
+
+    document.body.removeChild(textarea);  // Remove the textarea
+}
